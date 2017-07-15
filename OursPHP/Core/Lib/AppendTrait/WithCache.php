14d7874d18d77bc8ceeeef666c0621758a22a549
zhj2300380 <zhj_hk@163.com>
2017-07-15 22:13:27 +0800

    修复未配置withcache 导致参数错误的bug

--- OursPHP/Core/Lib/AppendTrait/WithCache.php
+++ OursPHP/Core/Lib/AppendTrait/WithCache.php
@@ -24,22 +24,31 @@ trait WithCache
      */
     public function __call($name, $params)
     {
-        $options=ConfigManage::getConfig('withcache',WEB_NAMESPACE);
         if (substr($name, -11) == '_with_cache') {
-
+            $options=ConfigManage::getConfig('withcache',WEB_NAMESPACE);
             $relFunc = substr($name, 0, strlen($name)-11);
             $key = md5(get_class($this).$name.serialize($params));
-            $cache_time_params = isset($params[0])? $params[0]:null;
+
 
 
             if($options==false || $options['used']==false)
             {
+                $cache_time_params = isset($params[0])? $params[0]:null;
+                if (is_string($cache_time_params) && substr($cache_time_params, 0, 11) == 'cache_time=') {
+                    array_shift($params); //第一个参数是缓存时间不是函数用的，去除
+                }
+                $cache_key_params = isset($params[0])? $params[0]:null;
+                if (is_string($cache_key_params) && substr($cache_key_params, 0, 10) == 'cache_key=') {
+                    array_shift($params); //第一个参数是缓存key不是函数用的，去除
+                }
                 return call_user_func_array(array($this, $relFunc), $params);
             }
 
             $cache_time = $options['cachetime'];
             $cache_type=$options['type'];
             $cache_nodename=$options['nodename'];
+
+            $cache_time_params = isset($params[0])? $params[0]:null;
             if (is_string($cache_time_params) && substr($cache_time_params, 0, 11) == 'cache_time=') {
                 $cache_time = intval(substr($cache_time_params, 11));
                 array_shift($params); //第一个参数是缓存时间不是函数用的，去除
@@ -47,7 +56,7 @@ trait WithCache
             $cache_key_params = isset($params[0])? $params[0]:null;
             if (is_string($cache_key_params) && substr($cache_key_params, 0, 10) == 'cache_key=') {
                 $key = substr($cache_key_params, 10);
-                array_shift($params); //第一个参数是缓存时间不是函数用的，去除
+                array_shift($params); //第一个参数是缓存key不是函数用的，去除
             }
             if('memcached'===$cache_type)
             {
@@ -60,8 +69,5 @@ trait WithCache
                 return $data;
             }
         }
-        $data =  call_user_func_array(array($this, $relFunc), $params);
-        return $data;
-
     }
 }
